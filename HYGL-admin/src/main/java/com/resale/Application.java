38874d34e316295e360eaeefec4c75d4760b391f
boyy3883 <976943455@qq.com>
2019-08-10 12:37:49 +0800

    XIUGAI

--- HYGL-admin/src/main/java/com/resale/Application.java
+++ HYGL-admin/src/main/java/com/resale/Application.java
@@ -2,13 +2,8 @@ package com.resale;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.EnableMBeanExport;
-import org.springframework.jmx.support.RegistrationPolicy;
-import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
-@EnableMBeanExport(registration = RegistrationPolicy.IGNORE_EXISTING)
 @MapperScan("com.resale.background.mapper")
-@EnableScheduling
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
--- HYGL-admin/src/main/java/com/resale/background/controller/EmployeeController.java
+++ HYGL-admin/src/main/java/com/resale/background/controller/EmployeeController.java
@@ -1,4 +1,5 @@
 package com.resale.background.controller;
+
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.HashMap;
@@ -28,236 +29,245 @@ import com.resale.background.util.ReturnMsgData;
 import com.resale.util.StringUtil;
 import com.resale.util.UUIDUtil;
 
-
 /**
- *<dl>
- *<dt>类名：EmployeeController.java</dt>
- *<dd>描述:员工管理逻辑实现</dd> 
- *<dd>创建时间： 2018年8月6日 下午5:33:38</dd>
- *<dd>创建人： TieGuowei </dd>
- *<dt>版本历史: </dt>
+ * <dl>
+ * <dt>类名：EmployeeController.java</dt>
+ * <dd>描述:员工管理逻辑实现</dd>
+ * <dd>创建时间： 2018年8月6日 下午5:33:38</dd>
+ * <dd>创建人： TieGuowei</dd>
+ * <dt>版本历史:</dt>
+ * 
  * <pre>
  * Date         Author      Version     Description 
  * ------------------------------------------------------------------ 
- * 2018年8月6日 下午5:33:38    TieGuowei       1.0        1.0 Version 
+ * 2018年8月6日 下午5:33:38    TieGuowei       1.0        1.0 Version
  * </pre>
- *</dl>
+ * </dl>
  */
 @Controller
 @RequestMapping("/employee")
-public class  EmployeeController extends BaseController{
+public class EmployeeController extends BaseController {
 
 	@Autowired
 	private EmployeeService employeeService;
-	
-    private Logger logger = LoggerFactory.getLogger(this.getClass());
 
-	   /**
-		 * 跳转到员工列表页面
-		 * @return
-		 */
-		@RequiresPermissions("employeeManager:list")//权限管理;
-		@RequestMapping("/goEmployeePage")
-		public String goEmployeePage(){
-			return "system/employee/employeeList";
-		}
-		
-		/**
-		 * 分页查询所有员工
-		 * @param request
-		 * @param dataMsg
-		 * @return
-		 */
-		@ResponseBody
-		@RequestMapping("/getEmployeeList")
-		public ReturnMsgData getEmployeeList(HttpServletRequest request,DataMsg dataMsg){
-			try {
-				Map<String, Object> paramsCondition = new HashMap<String, Object>();
-				Employee employee = getCurrentEmployee();
-				String employeeNo = StringUtil.trim(request.getParameter("employeeNo"));
-				if (StringUtil.isNotBlank(employeeNo)) {
-					paramsCondition.put("employeeNo", employeeNo);
-				}else if(!"admin".equals(employee.getEmployeeNo())){
-					paramsCondition.put("employeeNo", employee.getEmployeeNo());
-				}
-				String employeeName =  StringUtil.trim(request.getParameter("employeeName"));
-				if (StringUtil.isNotBlank(employeeName)) {
-					paramsCondition.put("employeeName", employeeName);
-				}
-				paramsCondition.put("pageNo", Integer.valueOf(request.getParameter("pageNumber")));
-				paramsCondition.put("pageSize", Integer.valueOf(request.getParameter("pageSize")));
-				PageModel pageModel =employeeService.getEmployeeList(paramsCondition);
-				dataMsg.setTotal(pageModel.getTotalRecords());
-				dataMsg.setRows(pageModel.getList());
-				new ReturnMsgData("success","0",dataMsg);
-			} catch (Exception e) {
-				e.printStackTrace();
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	/**
+	 * 跳转到员工列表页面
+	 * 
+	 * @return
+	 */
+	@RequiresPermissions("employeeManager:list") // 权限管理;
+	@RequestMapping("/goEmployeePage")
+	public String goEmployeePage() {
+		return "system/employee/employeeList";
+	}
+
+	/**
+	 * 分页查询所有员工
+	 * 
+	 * @param request
+	 * @param dataMsg
+	 * @return
+	 */
+	@ResponseBody
+	@RequestMapping("/getEmployeeList")
+	public ReturnMsgData getEmployeeList(HttpServletRequest request, DataMsg dataMsg) {
+		try {
+			Map<String, Object> paramsCondition = new HashMap<String, Object>();
+			Employee employee = getCurrentEmployee();
+			String employeeNo = StringUtil.trim(request.getParameter("employeeNo"));
+			if (StringUtil.isNotBlank(employeeNo)) {
+				paramsCondition.put("employeeNo", employeeNo);
+			} else if (!"admin".equals(employee.getEmployeeNo())) {
+				paramsCondition.put("employeeNo", employee.getEmployeeNo());
 			}
-			return new ReturnMsgData("error","1",dataMsg);
-		}
-		
-		/**
-		 * 查询员工拥有的角色
-		 * @param id
-		 * @return
-		 */
-		@ResponseBody
-		@RequestMapping("/getRole")
-		public Map<String,Object> getRole(@RequestParam("employeeId")int employeeId){
-			try {
-				List<Role>rlist=employeeService.getRoleList();
-				List<Integer> employeeRoleList=employeeService.getRoleByEmployeeId(employeeId);
-				Map<String,Object> map=new HashMap<String, Object>();
-				map.put("role", rlist);
-				map.put("userRole", employeeRoleList);
-				return map;
-			} catch (Exception e) {
-				logger.error(e.getMessage());
+			String employeeName = StringUtil.trim(request.getParameter("employeeName"));
+			if (StringUtil.isNotBlank(employeeName)) {
+				paramsCondition.put("employeeName", employeeName);
 			}
+			paramsCondition.put("pageNo", Integer.valueOf(request.getParameter("pageNumber")));
+			paramsCondition.put("pageSize", Integer.valueOf(request.getParameter("pageSize")));
+			PageModel pageModel = employeeService.getEmployeeList(paramsCondition);
+			dataMsg.setTotal(pageModel.getTotalRecords());
+			dataMsg.setRows(pageModel.getList());
+			new ReturnMsgData("success", "0", dataMsg);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return new ReturnMsgData("error", "1", dataMsg);
+	}
+
+	/**
+	 * 查询员工拥有的角色
+	 * 
+	 * @param id
+	 * @return
+	 */
+	@ResponseBody
+	@RequestMapping("/getRole")
+	public Map<String, Object> getRole(@RequestParam("employeeId") int employeeId) {
+		try {
+			List<Role> rlist = employeeService.getRoleList();
+			List<Integer> employeeRoleList = employeeService.getRoleByEmployeeId(employeeId);
+			Map<String, Object> map = new HashMap<String, Object>();
+			map.put("role", rlist);
+			map.put("userRole", employeeRoleList);
+			return map;
+		} catch (Exception e) {
+			logger.error(e.getMessage());
+		}
+		return null;
+	}
+
+	/**
+	 * 校验商户原始密码是否正确
+	 * 
+	 * @return
+	 */
+	@RequestMapping("/checkOldPwd")
+	@ResponseBody
+	public boolean checkOldPwd(@RequestParam("employeeId") int employeeId, @RequestParam("oldPwd") String oldPwd) {
+		try {
+			Employee employee = employeeService.getEmployeeById(employeeId);
+
+			String newPs = new SimpleHash("MD5", oldPwd, employee.getEmployeeNo() + employee.getSalt(), 2).toHex();
+			return employeeService.checkOldPwd(employeeId, oldPwd, newPs);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+	/**
+	 * 修改员工密码
+	 * 
+	 * @return
+	 */
+	@RequestMapping("/updatePwd")
+	@ResponseBody
+	public boolean updatePwd(@RequestParam("employeeId") int employeeId, @RequestParam("newPwd") String newPwd) {
+		try {
+			Employee employee = employeeService.getEmployeeById(employeeId);
+			String newPs = new SimpleHash("MD5", newPwd, employee.getEmployeeNo() + employee.getSalt(), 2).toHex();
+			employeeService.updatePwd(employeeId, newPwd, newPs);
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+	/**
+	 * 修改员工角色
+	 * 
+	 * @param merchantId
+	 * @param rids
+	 * @return
+	 */
+	@RequiresPermissions("employeeManager:updateRole") // 权限管理;
+	@ResponseBody
+	@RequestMapping("/updateEmployeeRole")
+	public boolean updateEmployeeRole(@RequestParam("employeeId") int employeeId, @RequestParam("rid") String rids) {
+		try {
+			employeeService.updateEmployeeRole(employeeId, rids);
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+	/**
+	 * 添加员工
+	 * 
+	 * @return
+	 */
+	@RequiresPermissions("employeeManager:add") // 权限管理;
+	@RequestMapping("/saveEmployee")
+	@ResponseBody
+	public boolean saveEmployee(Employee employee, @RequestParam("entryDate") String entryDate) {
+		try {
+
+			String uuid = UUIDUtil.getUUID();
+			String newPs = new SimpleHash("MD5", employee.getPassword(), employee.getEmployeeNo() + uuid, 2).toHex();
+			Subject subject = SecurityUtils.getSubject();
+			Employee employeeShiro = (Employee) subject.getPrincipal();
+			DateFormat date = new SimpleDateFormat("yyyy-MM-dd");
+			if (StringUtil.isNotEmpty(entryDate)) {
+				employee.setEntryTime(date.parse(entryDate));
+			}
+			employee.setOperator(employeeShiro.getEmployeeId());
+
+			employeeService.saveEmployee(employee, newPs, uuid);
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+	/**
+	 * 修改员工 回显
+	 * 
+	 * @return
+	 */
+	@RequestMapping("/getEmployeeById")
+	@ResponseBody
+	public Map<String, Object> getEmployeeById(@RequestParam("employeeId") int employeeId) {
+		try {
+			Map<String, Object> map = new HashMap<String, Object>();
+			Employee employee = employeeService.getEmployeeById(employeeId);
+			map.put("employee", employee);
+			return map;
+		} catch (Exception e) {
+			e.printStackTrace();
 			return null;
 		}
-		
-		/**
-		 * 校验商户原始密码是否正确
-		 * @return
-		 */
-		@RequestMapping("/checkOldPwd")
-		@ResponseBody
-		public boolean checkOldPwd(@RequestParam("employeeId")int employeeId,@RequestParam("oldPwd") String oldPwd){
-			try {
-				Employee employee = employeeService.getEmployeeById(employeeId);
-				
-				String newPs = new SimpleHash("MD5", oldPwd, employee.getEmployeeNo()+employee.getSalt(), 2).toHex();
-				return  employeeService.checkOldPwd(employeeId,oldPwd,newPs);
-			} catch (Exception e) {
-				e.printStackTrace();
-				return false;
+	}
+
+	/**
+	 * 修改员工信息
+	 * 
+	 * @return
+	 */
+	@RequiresPermissions("employeeManager:update") // 权限管理;
+	@RequestMapping("/updateEmployee")
+	@ResponseBody
+	public boolean updateEmployee(Employee employee, @RequestParam("entryDate") String entryDate) {
+		try {
+			Employee result = employeeService.getEmployeeById(employee.getEmployeeId());
+			String newPs = new SimpleHash("MD5", "123456", result.getEmployeeNo() + result.getSalt(), 2).toHex();
+			DateFormat date = new SimpleDateFormat("yyyy-MM-dd");
+			if (StringUtil.isNotEmpty(entryDate)) {
+				employee.setEntryTime(date.parse(entryDate));
 			}
+			employeeService.updateEmployee(employee, newPs);
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
 		}
-		
-		
-		/**
-		 * 修改员工密码
-		 * @return
-		 */
-		@RequestMapping("/updatePwd")
-		@ResponseBody
-		public boolean updatePwd(@RequestParam("employeeId")int employeeId,@RequestParam("newPwd") String newPwd){
-			try {
-				Employee employee = employeeService.getEmployeeById(employeeId);
-				String newPs = new SimpleHash("MD5", newPwd, employee.getEmployeeNo()+employee.getSalt(), 2).toHex();
-				employeeService.updatePwd(employeeId,newPwd,newPs);
-				 return true;
-			} catch (Exception e) {
-				e.printStackTrace();
-				return false;
-			}
+	}
+
+	/**
+	 * 更改员工状态（1：启用 2：删除 3：禁用）
+	 * 
+	 * @param merchant
+	 * @return
+	 */
+	@RequiresPermissions("employeeManager:delete") // 权限管理;
+	@ResponseBody
+	@RequestMapping("/deleteEmployee")
+	public boolean deleteEmployee(Employee employee, @RequestParam("activatedState") String activatedState) {
+		try {
+			employeeService.deleteEmployee(employee, activatedState);
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
 		}
-		
-		/**
-		 * 修改员工角色
-		 * @param merchantId
-		 * @param rids
-		 * @return
-		 */
-		@RequiresPermissions("employeeManager:updateRole")//权限管理;
-		@ResponseBody
-		@RequestMapping("/updateEmployeeRole")
-		public boolean updateEmployeeRole(@RequestParam("employeeId")int employeeId,@RequestParam("rid")String rids){
-			try {
-				employeeService.updateEmployeeRole(employeeId, rids);
-				return true;
-			} catch (Exception e) {
-				e.printStackTrace();
-				return false;
-			}
-		}
-		
-		/**
-		 * 添加员工
-		 * @return
-		 */
-		@RequiresPermissions("employeeManager:add")//权限管理;
-		@RequestMapping("/saveEmployee")
-		@ResponseBody
-		public boolean saveEmployee(Employee employee ,@RequestParam("entryDate")String entryDate){
-			try {
-				
-				String uuid = UUIDUtil.getUUID();
-			    String newPs = new SimpleHash("MD5", employee.getPassword(), employee.getEmployeeNo()+uuid, 2).toHex();
-			    Subject subject = SecurityUtils.getSubject();
-				Employee employeeShiro = (Employee) subject.getPrincipal();
-				DateFormat date = new SimpleDateFormat("yyyy-MM-dd");
-				if(StringUtil.isNotEmpty(entryDate) ){
-					employee.setEntryTime(date.parse(entryDate));    
-				}
-				employee.setOperator(employeeShiro.getEmployeeId());
-				
-				employeeService.saveEmployee(employee,newPs,uuid);
-				 return true;
-			} catch (Exception e) {
-				e.printStackTrace();
-				return false;
-			}
-		}
-		/**
-		 * 修改员工 回显
-		 * @return
-		 */
-		@RequestMapping("/getEmployeeById")
-		@ResponseBody
-		public Map<String,Object> getEmployeeById(@RequestParam("employeeId")int employeeId){
-			try {
-				Map<String,Object>map=new HashMap<String, Object>();
-				 Employee employee = employeeService.getEmployeeById(employeeId);
-				 map.put("employee", employee);
-				 return map;
-			} catch (Exception e) {
-				e.printStackTrace();
-				return null;
-			}
-		}
-		
-		/**
-		 * 修改员工信息
-		 * @return
-		 */
-		@RequiresPermissions("employeeManager:update")//权限管理;
-		@RequestMapping("/updateEmployee")
-		@ResponseBody
-		public boolean updateEmployee(Employee employee,@RequestParam("entryDate")String entryDate){
-			try {
-				Employee result =employeeService.getEmployeeById(employee.getEmployeeId());
-				String newPs = new SimpleHash("MD5", "123456", result.getEmployeeNo()+result.getSalt(), 2).toHex();
-				DateFormat date = new SimpleDateFormat("yyyy-MM-dd"); 
-				if(StringUtil.isNotEmpty(entryDate)){
-					employee.setEntryTime(date.parse(entryDate));   
-				}
-				employeeService.updateEmployee(employee,newPs);
-				 return true;
-			} catch (Exception e) {
-				e.printStackTrace();
-				return false;
-			}
-		}
-		
-		
-		/**
-		 * 更改员工状态（1：启用 2：删除 3：禁用）
-		 * @param merchant
-		 * @return
-		 */
-		@RequiresPermissions("employeeManager:delete")//权限管理;
-		@ResponseBody
-		@RequestMapping("/deleteEmployee")
-		public boolean deleteEmployee(Employee employee,@RequestParam("activatedState")String activatedState){ 
-			try {
-				employeeService.deleteEmployee(employee,activatedState);
-				return true;
-			} catch (Exception e) {
-				e.printStackTrace();
-				return false;
-			}
-			
-		}
+
+	}
 }
